cad5c76880bbc77390bee60f70ea3ffac1a1db24
Mauricio Diaz <mrcdz@pm.me>
2021-11-11 08:02:56 -0700

    Add Binary method

--- BaseConversion.cs
+++ BaseConversion.cs
@@ -6,11 +6,34 @@ namespace Activity5._2._2
     {
         private void Binary(int number)
         {
-            //toDo
+            string binary = "";
+            int remainder;
+            int decimalNumber = number;
+            do{
+                remainder = decimalNumber % 2;
+                if(remainder != 0)
+                {
+                    binary += "1 ";
+                } 
+                else 
+                {
+                    binary += "0 ";
+                }
+                decimalNumber = decimalNumber/2;
+            }while(decimalNumber > 0);
+            Console.WriteLine($"Decimal value {number} in Binary is: {Reverse(binary)}");
         }
         private void Octal(int number)
         {
-           //ToDo
+           string octal;
+           int remainder, quotient;
+           int decimalNumber = number;
+           do{
+               quotient = decimalNumber/8;
+               remainder = number - quotient*8;
+               octal += (string)
+
+           }while(decimalNumber > 0);
         }
         private void PrintHexDigit(int number)
         {
@@ -18,7 +41,31 @@ namespace Activity5._2._2
         }
         public void Print(int value, int nbase=10)
         {
-            //ToDo
+            switch(nbase)
+            {
+                case 2: Binary(value);
+                break;
+
+                case 8: Octal(value);
+                break;
+
+                case 16: PrintHexDigit(value);
+                break;
+
+                default: Console.WriteLine($"Decimal value: {value}");
+                break;
+            }
+        }
+        private static string Reverse(string number)
+        {
+            string[] digits = number.TrimEnd().Split();
+            string reversedString = "";
+
+            for(int i = digits.Length-1; i >= 0; i--)
+            {
+                reversedString += digits[i];
+            }
+            return reversedString;
         }
     }    
 }
